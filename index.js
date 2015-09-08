505f91cf33885d202d2a39ce530d3e52eb9eae5d
Andrew Wilkinson <awilkinson@Andrews-MacBook-Pro-3.local>
2015-09-08 16:53:36 +0100

    Pulls out the Z words but functions need separating

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
 module.exports = {
-  isogramChecker: function(word){
-    var isIsogram = true;
-    if (word.length > 0){
-      for(var i=0; i<word.length; i++){
-        var j = i + 1;
-        if (word[i] == word[j]){
-          isIsogram = false;
-          break;
+  isogramChecker: function(words){
+
+    var isoCount = [];
+
+    var checkAWord = function(word){
+      var isIsogram = true;
+      if (word.length > 0){
+        for(var i=0; i<word.length; i++){
+          var j = i + 1;
+          if (word[i] == word[j]){
+            isIsogram = false;
+            break;
+          };
         };
       };
-    };
-    return isIsogram;
+      return isIsogram;
+    }
+
+    if (words instanceof Array){
+      for (var i=0; i<words.length; i++){
+        if (checkAWord(words[i]) == true){
+          isoCount.push(words[i]);
+        };
+      };
+    }
+    return isoCount;
   },
 };
-
-
-
-
-//  scrabbleChecker: function(words){
-//    isoCount = [];
-//    if (words instanceof Array){
-//      for (var i=0; i<words.length; i++){
-//        if (isogramChecker(words[i]) == true){
-//          isoCount.push(words[i]);
-//        };
-//      };
-//    };
-//  }
